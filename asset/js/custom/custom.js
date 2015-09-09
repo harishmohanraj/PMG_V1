fbdd8b098a57c0bf429354230f2be492d298d4ef
Harish Mohan Raj <harishuideveloper@outlook.com>
2015-09-09 12:31:17 +0530

    latest code is pushed

--- asset/js/custom/custom.js
+++ asset/js/custom/custom.js
@@ -2,78 +2,83 @@ jQuery(document).ready(function() {
 
 
 
-					jQuery('.tp-banner').show().revolution(
-					{
-						dottedOverlay:"none",
-						delay:9000,
-						startwidth:1170,
-						startheight:700,
-						hideThumbs:200,
+      	jQuery('.tp-banner').show().revolution(
+      	{
+      		dottedOverlay:"none",
+      		delay:5000,
+      		startwidth:1170,
+      		startheight:700,
+      		hideThumbs:200,
 
-						thumbWidth:100,
-						thumbHeight:50,
-						thumbAmount:5,
+      		thumbWidth:100,
+      		thumbHeight:50,
+      		thumbAmount:5,
 
-						navigationType:"none",
-						navigationArrows:"solo",
-						navigationStyle:"preview3",
+      		navigationType:"bullet",
+      		navigationArrows:"solo",
+      		navigationStyle:"preview1",
 
-						touchenabled:"on",
-						onHoverStop:"on",
+      		touchenabled:"on",
+      		onHoverStop:"on",
 
-						swipe_velocity: 0.7,
-						swipe_min_touches: 1,
-						swipe_max_touches: 1,
-						drag_block_vertical: false,
+      		swipe_velocity: 0.7,
+      		swipe_min_touches: 1,
+      		swipe_max_touches: 1,
+      		drag_block_vertical: false,
 
+      								parallax:"mouse",
+      		parallaxBgFreeze:"on",
+      		parallaxLevels:[7,4,3,2,5,4,3,2,1,0],
 
-						keyboardNavigation:"on",
+      		keyboardNavigation:"off",
 
-						navigationHAlign:"center",
-						navigationVAlign:"bottom",
-						navigationHOffset:0,
-						navigationVOffset:20,
+      		navigationHAlign:"center",
+      		navigationVAlign:"bottom",
+      		navigationHOffset:0,
+      		navigationVOffset:20,
 
-						soloArrowLeftHalign:"left",
-						soloArrowLeftValign:"center",
-						soloArrowLeftHOffset:20,
-						soloArrowLeftVOffset:0,
+      		soloArrowLeftHalign:"left",
+      		soloArrowLeftValign:"center",
+      		soloArrowLeftHOffset:20,
+      		soloArrowLeftVOffset:0,
 
-						soloArrowRightHalign:"right",
-						soloArrowRightValign:"center",
-						soloArrowRightHOffset:20,
-						soloArrowRightVOffset:0,
+      		soloArrowRightHalign:"right",
+      		soloArrowRightValign:"center",
+      		soloArrowRightHOffset:20,
+      		soloArrowRightVOffset:0,
 
-						shadow:0,
-						fullWidth:"on",
-						fullScreen:"off",
+      		shadow:0,
+      		fullWidth:"on",
+      		fullScreen:"off",
 
-						spinner:"spinner0",
+      		spinner:"spinner4",
 
-						stopLoop:"off",
-						stopAfterLoops:-1,
-						stopAtSlide:-1,
+      		stopLoop:"off",
+      		stopAfterLoops:-1,
+      		stopAtSlide:-1,
 
-						shuffle:"off",
+      		shuffle:"off",
 
-						autoHeight:"off",
-						forceFullWidth:"off",
-
-
-
-						hideThumbsOnMobile:"off",
-						hideNavDelayOnMobile:1500,
-						hideBulletsOnMobile:"off",
-						hideArrowsOnMobile:"off",
-						hideThumbsUnderResolution:0,
-
-						hideSliderAtLimit:0,
-						hideCaptionAtLimit:0,
-						hideAllCaptionAtLilmit:0,
-						startWithSlide:0
-					});
+      		autoHeight:"off",
+      		forceFullWidth:"off",
 
 
 
+      		hideThumbsOnMobile:"off",
+      		hideNavDelayOnMobile:1500,
+      		hideBulletsOnMobile:"off",
+      		hideArrowsOnMobile:"off",
+      		hideThumbsUnderResolution:0,
 
-				});	//ready
+      		hideSliderAtLimit:0,
+      		hideCaptionAtLimit:0,
+      		hideAllCaptionAtLilmit:0,
+      		startWithSlide:0,
+      		videoJsPath:"rs-plugin/videojs/",
+      		fullScreenOffsetContainer: ""
+      	});
+
+
+
+
+      });	//ready
